56614a11bb4f334f2d66da8721a5beb7b177fdbe
KEbad2008 <ebaduk117@gmail.com>
2026-05-30 15:21:45 -0400

    intital

---
+++ README.md
@@ -0,0 +1,7 @@
+This is my first python project ever.
+
+I am writing some code for a try yourself exercise project from free code camp.
+
+I am learning how to use python in visual studio code aswell.
+
+the code can add/sub/multiply/divide any 2 numbres from 1-9 that you enter!
---
+++ firstproject.py
@@ -0,0 +1,21 @@
+# my first python project ever.
+
+# gonna be about basic arithmatic when user enters numbers
+
+number_1 = int(input("Please Enter a Number between 1 and 9: "))
+number_2 = int(input("Please Enter another number between 1 and 9: "))
+
+add = (number_1 + number_2)
+subtract = (number_1 - number_2)
+multiply = (number_1 * number_2)
+divide = (number_1 / number_2)
+exponent = (number_1 ** number_2)
+
+print(add)
+print(subtract)
+print(multiply)
+print(divide)
+print(exponent)
+
+print("\n-----------------------\n")
+
