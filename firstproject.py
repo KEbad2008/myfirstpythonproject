0920fb9e1ef797804bf0a1045784a468c89096bd
KEbad2008 <ebaduk117@gmail.com>
2026-06-03 19:14:54 -0400

    addded sum new stuff

--- firstproject.py
+++ firstproject.py
@@ -10,12 +10,32 @@ subtract = (number_1 - number_2)
 multiply = (number_1 * number_2)
 divide = (number_1 / number_2)
 exponent = (number_1 ** number_2)
+modulus = number_1 % number_2
 
-print(add)
-print(subtract)
-print(multiply)
-print(divide)
-print(exponent)
+
+
+print("Addition:", add)
+print("Subtraction:", subtract)
+print("Multiplication:", multiply)
+print("Division:", divide)
+print("Exponent:", exponent)
+print("Remainder:", modulus)
+
+# this is to tell user which of their numbers are bigger!
+if number_1 > number_2:
+    print(number_1, "is larger.")
+elif number_2 > number_1:
+    print(number_2, "is larger.")
+else:
+    print("The numbers are equal.")
+
+
+#avg of numbers
+average = (number_1 + number_2) / 2
+
+print("Average:", average)
 
 print("\n-----------------------\n")
-
+print()
+print()
+print("Thank you for using this program! I hope you enjoyed it!")
